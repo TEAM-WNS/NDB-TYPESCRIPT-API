0bbdf5a80608a2b2e893578ab5efa1222467f31f
fireing123 <gimd82368@gmail.com>
2024-08-22 23:04:18 +0900

    check

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { NDBRespone } from "./types";
 
 export class NDB {
-    fetchUser: string;
+    fetchUser: string = '';
 
-    constructor(public url: string, private userId: string, private password: string) {
-        // login check
+    constructor(public url: string) {
+    }
+
+    public async init(userId: string, password: string) {
         this.fetchUser = `id=${userId}&pw=${password}`;
+        const res = await fetch(`${this.url}/check?${this.fetchUser}}`)
+            .then(async res => await res.json()) as NDBRespone;
+        if (!('result' in res && res.result == 'exist')) {
+            throw Error("계정이 없음!");
+        }
     }
 
     public getDataBase(dbName: string): DataBase {
@@ -13,11 +20,14 @@ export class NDB {
         return database
     }
 
-    public async createDataBase(dbName: string, collectionName: string): Promise<DataBase> {
-        // fetch
-        const r = await fetch(`${this.url}/create?${this.fetchUser}&database=${dbName}&collection=${collectionName}`)
-        // throw error
-        return this.getDataBase(dbName);
+    public async createDataBase(dbName: string): Promise<DataBase> {
+        const res = await fetch(`${this.url}/create?${this.fetchUser}&database=${dbName}&collection=Default`)
+            .then(async res => await res.json()) as NDBRespone;
+        if ('result' in res && res.result == 'fail') {
+            throw Error(res.cause);
+        } else {
+            return this.getDataBase(dbName);
+        }
     }
 }
 
@@ -29,9 +39,14 @@ class DataBase {
         }
     }
 
-    public static hasDataBase(client: NDB, name: string): boolean {
-        // has check
-        return false;
+    public static async hasDataBase(client: NDB, name: string): Promise<boolean> {
+        const res = await fetch(`${client.url}/check?${client.fetchUser}&database=${name}&collection=Default`)
+            .then(async res => await res.json()) as NDBRespone;
+        if ('result' in res && res.result == 'ALREADY_EXIST') {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public getCollection(name: string): Collection {
@@ -39,10 +54,15 @@ class DataBase {
         return collection;
     }
 
-    public createCollection(name: string): Collection {
-        // fetch
-        
-        return this.getCollection(name);
+    public async createCollection(name: string): Promise<Collection> {
+        const res = await fetch(`${this.client.url}/create?${this.client.fetchUser}&database=${this.name}&collection=${name}`)
+            .then(async res => await res.json()) as NDBRespone;
+        if ('result' in res && res.result == 'fail') {
+            throw Error(res.cause);
+        } else {
+            return this.getCollection(name);
+        }
+
     }
 }
 
@@ -58,16 +78,21 @@ class Collection {
         this.nameParams = `database=${database.name}&collection=${name}`;
     }
 
-    public static hasCollection(database: DataBase, name: string): boolean {
-        // has check
-        return false;
+    public static async hasCollection(database: DataBase, name: string): Promise<boolean> {
+        const res = await fetch(`${database.client.url}/check?${database.client.fetchUser}&database=${database.name}&collection=${name}`)
+            .then(async res => await res.json()) as NDBRespone;
+        if ('result' in res && res.result == 'ALREADY_EXIST') {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public async NDBfetch(input: string, init?: RequestInit): Promise<NDBRespone | object> {
         return await fetch(`${this.database.client.url}/collection?${this.fetchUser}&${this.nameParams}${input}`, init).then(async res => await res.json());
     }
 
-    public async findBy(key: string) {
+    public async find(key: string) {
         const res = await this.NDBfetch(`&findBy=${key}`);
         if ('result' in res && res.result == 'fail') {
             throw Error(res.cause);
@@ -76,7 +101,7 @@ class Collection {
         }
     }
 
-    public async editValue(key: string, value: object) {
+    public async edit(key: string, value: object) {
         const res = await this.NDBfetch(`&findBy=${key}&edit=${JSON.stringify(value)}`);
         if ('result' in res && res.result == 'fail') {
             throw Error(res.cause);
@@ -85,15 +110,50 @@ class Collection {
         }
     }
 
-    public setStrict(key: string, value: boolean) {
-
+    public async add(key: string, value: object) {
+        const res = await this.NDBfetch(`&addKey=${key}&keyValue=${JSON.stringify(value)}`);
+        if ('result' in res && res.result == 'fail') {
+            throw Error(res.cause);
+        } else {
+            return true;
+        }
     }
 
-    public addKey(key: string, value: object) {
-
+    public async delete(key: string) {
+        const res = await this.NDBfetch(`&deleteKey=${key}`);
+        if ('result' in res && res.result == 'fail') {
+            throw Error(res.cause);
+        } else {
+            return true;
+        }
     }
 
-    public deleteKey(key: string) {
-
+    public async addFieldValue(key: string, fieldName: string, fieldValue: object, isStrict: boolean = false) {
+        const res = await this.NDBfetch(`&findBy=${key}&addField=${fieldName}&fieldValue=${fieldValue}&isStrict=${JSON.stringify(isStrict)}`);
+        if ('result' in res && res.result == 'fail') {
+            throw Error(res.cause);
+        } else {
+            return true;
+        }
     }
+
+    public async addFieldValueIsList(key: string, fieldName: string, fieldValue: object) {
+        const res = await this.NDBfetch(`&findBy=${key}&addField=${fieldName}&fieldValue=${fieldValue}`);
+        if ('result' in res && res.result == 'fail') {
+            throw Error(res.cause);
+        } else {
+            return true;
+        }
+    }
+
+    public async deleteFieldValue(key: string, fieldName: string) {
+        const res = await this.NDBfetch(`&findBy=${key}&deleteField=${fieldName}`);
+        if ('result' in res && res.result == 'fail') {
+            throw Error(res.cause);
+        } else {
+            return true;
+        }
+    }
+
+
 }
